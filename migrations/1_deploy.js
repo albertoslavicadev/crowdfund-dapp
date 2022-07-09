4fcedbca3db9faff7e67e47dc4c894772350b720
Alberto Slavica <104838972+hathateth@users.noreply.github.com>
2022-07-09 11:27:01 +0200

    Update 1_deploy.js

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,7 +1,7 @@
 const CROWDSALE = artifacts.require("CrowdFund");
 
 module.exports = async function (deployer) {
-    await deployer.deploy(CROWDSALE, "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7" );
+    await deployer.deploy(CROWDSALE, "(add here the token you want to crowdsale)" );
     const crowdSale = await CROWDSALE.deployed();
 
 };
